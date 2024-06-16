a9aa9f5cbd1b6476230bdc933611750075b4602c
namka <namarajkarki007@gmail.com>
2024-06-16 13:30:49 +0545

    vue project initiation

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useCommonStore } from "../stores/commonStore";
+const store = useCommonStore();
+</script>
+
+<template>
+  <main>
+    {{ store.test }}
+    <div class="text-2xl text-blue-500">
+      hello world
+    </div>
+  </main>
+</template>
